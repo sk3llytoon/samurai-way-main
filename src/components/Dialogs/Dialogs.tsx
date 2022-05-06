cbe631ee4496a076682c4d93c677d4675dc3c948
sk3llytoon <hatsz141415@gmail.com>
2022-05-06 20:25:15 +0300

    22/100 - done

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,47 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 import s from './Dialogs.module.css';
+
+type DialogItemProps = {
+    name: string;
+    id: string
+}
+
+type MessageProps={
+    sms: string;
+}
+
+const DialogItem = (props: DialogItemProps) => {
+    let path = "/dialogs/" + props.id;
+    return (
+        <div className={s.dialog}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const Message = (props:MessageProps) => {
+    return (
+        <div className={s.message}>{props.sms}</div>
+    )
+}
+
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <DialogItem name="German" id="1"/>
+                <DialogItem name="Natalya" id="2"/>
+                <DialogItem name="Tatyana" id="3"/>
+                <DialogItem name="Vadim" id="4"/>
+            </div>
+            <div className={s.messages}>
+                <Message sms="How is your day?"/>
+                <Message sms="You can do it, dont give up"/>
+                <Message sms="Lets gooo"/>
+                <Message sms="AAAUUUUUUGH"/>
+            </div>
         </div>
     )
 }
