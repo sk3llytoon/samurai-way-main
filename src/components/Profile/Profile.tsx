2a6335ee7b168ca8b58e3f4e6bd2977197f8c35d
sk3llytoon <hatsz141415@gmail.com>
2022-05-06 21:16:06 +0300

    23/100 - done

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import s from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
+import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
 export const Profile = () => {
     return (
         <div>
-            <div>
-                <img className={s.pic}
-                    src="https://images.unsplash.com/photo-1511300636408-a63a89df3482?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGVza3RvcCUyMHdhbGxwYXBlcnN8ZW58MHx8MHx8&w=1000&q=80"
-                    alt=""/>
-            </div>
-            <div>
-                ava + description
-            </div>
+            <ProfileInfo/>
             <MyPosts/>
         </div>
     )
