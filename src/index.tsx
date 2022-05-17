3469631490e96acab4b5f7f873e4474ce0d9e0d3
sk3llytoon <hatsz141415@gmail.com>
2022-05-17 22:52:47 +0300

    32/100 - done

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import {state} from './redux/state'
 import {addPost} from "./redux/state";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-
-    <App state={state} addPost={addPost}/>
-    ,
-  document.getElementById('root')
+    <BrowserRouter>
+        <App state={state} addPost={addPost}/>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
